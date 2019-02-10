9b1683355e70556a89c60ffe2b5ee39b365c7c5c
Andy Jung <40802254+appx-andyjung@users.noreply.github.com>
2019-02-10 15:25:41 -0500

    Updated for csv writing

--- poller.py
+++ poller.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import csv
+
 url = 'https://jsonplaceholder.typicode.com/posts/1'
 response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
 
@@ -9,7 +11,10 @@ jsondata = json.loads(text)
 
 if __name__ == "__main__":
 	jsondata['title']
-	print('Title data is', jsondata['title'])
-
-
-
+	
+	with open('test.csv','w') as csvfile:
+		fieldnames = ['title','body']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		writer.writerow({'title': jsondata['title'], 'body': jsondata['body']})
+	print('Completed')
